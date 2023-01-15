9d5d133a8e370c323f217e002ec072924c680ea3
Fabiano Junior <99301175+fabianojrpro@users.noreply.github.com>
2023-01-15 19:26:35 -0300

    Add files via upload

---
+++ ExercicioAreaDoCirculo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+double raioDoCirculo, pi, area;
+
+raioDoCirculo = double.Parse(Console.ReadLine());
+pi = 3.14159;
+
+area = pi * raioDoCirculo * raioDoCirculo;
+
+Console.WriteLine("A = "+area.ToString("F4", CultureInfo.InvariantCulture));
